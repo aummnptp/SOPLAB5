13f839c5db3582a8d83106ddc61aefe9fdaac6ab
aummnptp <88504541+aummnptp@users.noreply.github.com>
2023-09-11 21:03:20 +0700

    Add files via upload

--- Sentence.java
+++ Sentence.java
@@ -1,10 +1,34 @@
 package com.example.lab5rabbit;
 
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Sentence {
+
+public class Sentence implements Serializable {
     ArrayList<String>  badSentences = new ArrayList<String>();
     ArrayList<String>   goodSentences = new ArrayList<String>();
 
+    public ArrayList<String> getBadSentences() {
+        return badSentences;
+    }
 
+    public ArrayList<String> getGoodSentences() {
+        return goodSentences;
+    }
+        public Sentence() {
+    }
+
+    public void setBadSentences(ArrayList<String> badSentences) {
+        this.badSentences = badSentences;
+    }
+
+    public void setGoodSentences(ArrayList<String> goodSentences) {
+        this.goodSentences = goodSentences;
+    }
+    //    public Sentence(ArrayList<String> badSentences, ArrayList<String> goodSentences) {
+//        this.badSentences = badSentences;
+//        this.goodSentences = goodSentences;
+//    }
 }
